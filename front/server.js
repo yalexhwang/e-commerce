bffe199435de4d390ee4739681ea03a490c641ed
yalexhwang <y.alex.hwang@gmail.com>
2016-09-19 16:55:43 -0400

    removed

--- front/server.js
+++
@@ -1,6 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8000, function(){
-    console.log('Listening on port 8000...');
-});
-
